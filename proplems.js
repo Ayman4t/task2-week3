49f31bc8ec263b70ead4a8dd52b0c96ab7c4e371
unknown <salemaymen814@gmail.com>
2024-09-26 23:26:24 +0300

    ok

---
+++ proplems.js
@@ -0,0 +1,58 @@
+
+// problem 1
+
+// Filtering an Array
+
+// function evenonly(arr){
+//     var result = arr.filter((elem)=>elem%2==0)
+//     return result
+// }
+// arr=[1,2,3,4,5]
+// console.log(evenonly(arr))
+
+//////////////////////////////////////////////////////////////////
+
+// problem 2
+
+// Find the Largest Number
+
+// function largeonly(arr){
+//     var max=-1
+//     arr.forEach((elem)=>max=Math.max(max,elem))
+//     return max
+// }
+// arr=[1,2,3,4,20,5]
+// console.log(largeonly(arr))
+
+/////////////////////////////////////////////////////////////////////
+
+// problem 3
+
+//  Reverse a String
+
+// function reverse(str) {
+//     let result = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//       result += str[i];
+//     }
+//     return result;
+//   }
+
+//   let str = "ayman";
+//   console.log(reverse(str)); 
+
+////////////////////////////////////////////////////////////////////
+
+// problem 4
+
+//  Remove Duplicates
+
+// function onlyUnique(value, index, array) {
+//     return array.indexOf(value) === index;
+// }
+
+
+// arr = [1, 2, 3, 4, 20, 5, 2,3,4,9]
+// var unique = arr.filter(onlyUnique);
+
+// console.log(unique); 
